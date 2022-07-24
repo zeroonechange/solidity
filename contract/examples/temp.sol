85bd4502a4ad7424fc8691f194dea1b3fc9253e7
Remix Workspace <>
2022-07-25 01:31:02 +0800

    temp

---
+++ contract/examples/temp.sol
@@ -0,0 +1,30 @@
+    // SPDX-License-Identifier: GPL-3.0
+    pragma solidity >=0.4.0 <0.9.0;
+
+    contract A {
+        ...
+    }
+
+
+    contract B {
+        ...
+    }
+
+
+    contract C {
+        ...
+    }
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.0 <0.9.0;
+
+contract A {
+    ...
+}
+contract B {
+    ...
+}
+
+contract C {
+    ...
+}
