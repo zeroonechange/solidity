91c604515b25a95cd73e4ee79b3db137823d9ac4
Money Orientation Programming <zeroonechange@gmail.com>
2022-09-04 00:53:05 +0800

    add web.js at hardhat

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+// usePlugin("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-web3");
+// import "@nomiclabs/hardhat-web3";
+
+// task action function receives the Hardhat Runtime Environment as second argument
+task("accounts", "Prints accounts", async (_, { web3 }) => {
+  console.log(await web3.eth.getAccounts());
+});
+
 module.exports = {
   solidity: "0.8.9",
 };
