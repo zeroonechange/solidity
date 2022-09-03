e067c0bf7997b284f61d85e3d6c5f6c1e45e6d8c
Money Orientation Programming <zeroonechange@gmail.com>
2022-09-04 01:44:07 +0800

    set rinkeby env successfully

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,23 +1,16 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
 
-// usePlugin("@nomiclabs/hardhat-web3");
-require("@nomiclabs/hardhat-web3");
-// import "@nomiclabs/hardhat-web3";
 
-// task action function receives the Hardhat Runtime Environment as second argument
-task("accounts", "Prints accounts", async (_, { web3 }) => {
-  console.log(await web3.eth.getAccounts());
-});
+const ALCHEMY_API_KEY = "fcvu1qm3Vc7yAxt53kZ_5H9cHuEe8vCE";
+const GOERLI_PRIVATE_KEY = "a40f68e080b908bfe99210d483cf5aa53dd2bde380cfba94f3b2de4149e2a562";
 
 module.exports = {
-  defaultNetwork: "rinkeby",
-  networks: {
-    hardhat: {
-    },
-    rinkeby: { 
-      url: "https://rinkeby.infura.io/v3",
-      accounts: ['a40f68e080b908bfe99210d483cf5aa53dd2bde380cfba94f3b2de4149e2a562']
-    }
-  },
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
