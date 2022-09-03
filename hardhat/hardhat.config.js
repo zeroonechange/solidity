abff125e2267e96d7190487bb5852944e32f5adf
Money Orientation Programming <zeroonechange@gmail.com>
2022-09-04 00:58:55 +0800

    set rinkeby test environment

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -10,5 +10,14 @@ task("accounts", "Prints accounts", async (_, { web3 }) => {
 });
 
 module.exports = {
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+    },
+    rinkeby: { 
+      url: "https://rinkeby.infura.io/v3",
+      accounts: ['a40f68e080b908bfe99210d483cf5aa53dd2bde380cfba94f3b2de4149e2a562']
+    }
+  },
   solidity: "0.8.9",
 };
