1eb84e7dc34c376d4ca0b528f4609d9619c1f06a
Money Orientation Programming <zeroonechange@gmail.com>
2022-09-08 00:11:41 +0800

    npm install -D @nomiclabs/hardhat-waffle ethereum-waffle
    npx hardhat node
    npx hardhat run --network localhost scripts/deploy.ts

---
+++ hardhat/scripts/deploy.ts
@@ -0,0 +1,29 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = ethers.utils.parseEther("1");
+
+  const Lock = await ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+
+//  npx hardhat node
+//  npx hardhat run --network localhost scripts/deploy.ts
