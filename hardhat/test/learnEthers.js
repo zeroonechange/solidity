9c6297f6015e669b1aa565c811d424507fc08a82
Money Orientation Programming <30459094+zeroonechange@users.noreply.github.com>
2022-09-07 15:56:30 +0800

    Update learnEthers.js

--- hardhat/test/learnEthers.js
+++ hardhat/test/learnEthers.js
@@ -1,30 +1,33 @@
-const { expect } = require('chai');
-const { ethers } = require('hardhat');
+// const { expect } = require('chai');
+// const { ethers } = require('hardhat');
 
-describe('Demo', () => {
-    let deployer;
-    let demoContract;
+// describe('Demo', () => {
 
-    before(async () => {
-        [deployer] = await ethers.getSigners();
-        const factory = await ethers.getContractFactory('Demo');
-        demoContract = await factory.deploy().then((res) => res.deployed());
-    });
+//     let deployer;
+//     let demoContract;
 
-    it('should invoke the fallback function', async () => {
-        const nonExistentFuncSignature = 'nonExistentFunc(uint256,uint256)';
-        const fakeDemoContract = new ethers.Contract(
-            demoContract.address,
-            [
-                ...demoContract.interface.fragments,
-                `function ${nonExistentFuncSignature}`,
-            ],
-            deployer,
-        );
-        const tx = fakeDemoContract[nonExistentFuncSignature](8, 9);
-        await expect(tx)
-            .to.emit(demoContract, 'Error')
-            .withArgs('call of a non-existent function');
-    });
+//     before(async () => {
+//         [deployer] = await ethers.getSigners();
+//         const factory = await ethers.getContractFactory('Demo');
+//         // demoContract = await factory.deploy().then((res) => res.deployed());
+//         demoContract = await factory.deploy();
+//         await demoContract.deployed();
+//     });
 
-});
+//     it('should invoke the fallback function', async () => {
+//         const nonExistentFuncSignature = 'nonExistentFunc(uint256,uint256)';
+//         const fakeDemoContract = new ethers.Contract(
+//             demoContract.address,
+//             [
+//                 ...demoContract.interface.fragments,
+//                 `function ${nonExistentFuncSignature}`,
+//             ],
+//             deployer,
+//         );
+//         const tx = fakeDemoContract[nonExistentFuncSignature](8, 9);
+//         await expect(tx)
+//             .to.emit(demoContract, 'Error')
+//             .withArgs('call of a non-existent function');
+//     });
+
+// });
