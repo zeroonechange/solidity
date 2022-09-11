c2ed4b128f97934b16f206232d7c1c2aecfc616d
Money Orientation Programming <zeroonechange@gmail.com>
2022-09-11 13:52:13 +0800

    hardhat succeed deploy via V2_CNRedCross

--- hardhat/upgradeableDemo/test/V2_CNRedCross.ts
+++ hardhat/upgradeableDemo/test/V2_CNRedCross.ts
@@ -15,68 +15,71 @@ describe("V2_CNRedCross Test", function () {
 
     async function deployTokenFixture() {
         const [owner] = await ethers.getSigners();
+        console.log("owner -> " + owner.address)
 
         const Logic = await ethers.getContractFactory("Logic");
         const logic = await Logic.deploy();
         await logic.deployed();
 
-        const TransparentUpgradeableProxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
-        const proxy = await TransparentUpgradeableProxy.deploy();
-        await proxy.deployed();
+        console.log("logic contract deployed -> " + logic.address)
 
         const ProxyAdmin = await ethers.getContractFactory("ProxyAdmin");
         const proxyAdmin = await ProxyAdmin.deploy();
         await proxyAdmin.deployed();
 
+        console.log("proxyAdmin contract deployed -> " + proxyAdmin.address)
+
+        const TransparentUpgradeableProxy = await ethers.getContractFactory("TransparentUpgradeableProxy");
+        const proxy = await TransparentUpgradeableProxy.deploy(logic.address, proxyAdmin.address, "0x8129fc1c");
+        await proxy.deployed();
+
+        console.log("proxy contract deployed -> " + proxy.address)
+
         return { Logic, logic, proxy, proxyAdmin, owner };
     }
 
-    describe("Deployment", function () {
-
-        it("proxy Should set the right logic", async function () {
-            const { logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
-            await proxyAdmin.upgrade(proxy, logic)
-            expect(await proxyAdmin.getProxyImplementation(proxy).to.equal(logic.address));
-            expect(await proxyAdmin.getProxyAdmin(proxy).to.equal(owner.address));
-        });
-
-        /* it("call logic via proxy.fallback()", async function () {
-            const { Logic, logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
-
-            const fallbackProxy = await Logic.attach(proxy.address);
-
-            const setResp = await fallbackProxy.SetParam('a', 4);
-            expect(setResp).to.emit(proxy, "ParamSetEvent")
-                .withArgs('a', 4);
-            const getResp = await proxy.GetParam('a');
-            expect(getResp).to.emit(proxy, "ParamGetEvent")
-                .withArgs('a', 4);
-        });
-
-        it("upgrade logic contract", async function () {
-            const { Logic, logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
-
-            const LogicV2 = await ethers.getContractFactory("LogicV2");
-            const logicV2 = await LogicV2.deploy();
-            await logicV2.deployed();
-
-            await proxyAdmin.upgrade(proxy, logicV2)
-            expect(await proxyAdmin.getProxyImplementation(proxy).to.equal(logicV2.address));
-            expect(await proxyAdmin.getProxyAdmin(proxy).to.equal(owner.address));
-
-            const fallbackProxy = await LogicV2.attach(proxy.address);
-
-            // 之前设置了 a=4  GetParam 新代码 返回 a+10 = 14 
-            const getResp = await proxy.GetParam('a');
-            expect(getResp).to.emit(proxy, "ParamGetEvent")
-                .withArgs('a', 14);
-            // 重新设置 a=1   SetParam 代码没变  
-            const setResp = await fallbackProxy.SetParam('a', 1);
-            expect(setResp).to.emit(proxy, "ParamSetEvent")
-                .withArgs('a', 1);
-            // 再次获取 a 的值 应该是  1+10 = 11 
-            expect(getResp).to.emit(proxy, "ParamGetEvent")
-                .withArgs('a', 11);
-        }); */
+    it("proxy Should set the right logic", async function () {
+        const { logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
+        expect(await proxyAdmin.getProxyImplementation(proxy.address)).to.equal(logic.address)
+        expect(await proxyAdmin.getProxyAdmin(proxy.address)).to.equal(proxyAdmin.address)
     });
+
+    /* it("call logic via proxy.fallback()", async function () {
+        const { Logic, logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
+
+        const fallbackProxy = await Logic.attach(proxy.address);
+
+        const setResp = await fallbackProxy.SetParam('a', 4);
+        expect(setResp).to.emit(proxy, "ParamSetEvent")
+            .withArgs('a', 4);
+        const getResp = await proxy.GetParam('a');
+        expect(getResp).to.emit(proxy, "ParamGetEvent")
+            .withArgs('a', 4);
+    });
+
+    it("upgrade logic contract", async function () {
+        const { Logic, logic, proxy, proxyAdmin, owner } = await loadFixture(deployTokenFixture);
+
+        const LogicV2 = await ethers.getContractFactory("LogicV2");
+        const logicV2 = await LogicV2.deploy();
+        await logicV2.deployed();
+
+        await proxyAdmin.upgrade(proxy, logicV2)  //upgradeAndCall(proxy.address, logic.address, '0x8129fc1c')
+        expect(await proxyAdmin.getProxyImplementation(proxy).to.equal(logicV2.address));
+        expect(await proxyAdmin.getProxyAdmin(proxy).to.equal(owner.address));
+
+        const fallbackProxy = await LogicV2.attach(proxy.address);
+
+        // 之前设置了 a=4  GetParam 新代码 返回 a+10 = 14 
+        const getResp = await proxy.GetParam('a');
+        expect(getResp).to.emit(proxy, "ParamGetEvent")
+            .withArgs('a', 14);
+        // 重新设置 a=1   SetParam 代码没变  
+        const setResp = await fallbackProxy.SetParam('a', 1);
+        expect(setResp).to.emit(proxy, "ParamSetEvent")
+            .withArgs('a', 1);
+        // 再次获取 a 的值 应该是  1+10 = 11 
+        expect(getResp).to.emit(proxy, "ParamGetEvent")
+            .withArgs('a', 11);
+    }); */
 });
